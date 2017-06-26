b34648cced0e5d131cba2cffcfbca2c01ce92515
Troush <troush69@gmail.com>
2017-06-26 11:23:09 +0300

    Add return to test_adapter

--- lib/adapters/test_adepter.ex
+++ lib/adapters/test_adepter.ex
@@ -5,10 +5,12 @@ defmodule Mouth.TestAdapter do
   @behaviour Mouth.Adapter
   def deliver(message, _) do
     IO.puts message.body
+    {:ok, [status: "Accepted", id: "test"]}
   end
 
   def status(_, config) do
     IO.puts inspect config
+    {:ok, [status: "Accepted", id: "test"]}
   end
 
   def handle_config(config) do
